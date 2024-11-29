f2a3b5251cbe53fc47f6edd601d4dbadec506c6f
Rokas <rdidrikas@gmail.com>
2024-11-29 13:24:17 +0200

    Made invalid input handling in main

--- main.c
+++ main.c
@@ -15,7 +15,16 @@ int main(){
     printf("5. Deletes the structure\n");
     printf("6. Exit\n");
     printf("Enter your choice: ");
-    scanf("%d", &choice);
+    if(scanf("%d", &choice) == 1){
+        if(choice < 1 || choice > 6){
+            printf("Invalid input... Try again.\n");
+            main();
+        }
+    }
+    else{
+        printf("Invalid input... Try again.\n");
+        main();
+    }
     proccess_choice(choice);
 
 }
